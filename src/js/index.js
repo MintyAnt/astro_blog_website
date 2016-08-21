d8b49f6a82f41d04f65d1d5ea0c2e4eef80b2f51
Duane <duanese1@localhost.na.novartis.net>
2016-08-21 13:28:22 -0400

    linting stuff

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+/* global document */
 
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -11,4 +12,3 @@ ReactDOM.render(
     <Router history={history} routes={routes} />,
     document.getElementById('content')
 );
-
